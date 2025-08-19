319d0de77ab08b011178381869e2cdd141cbcf77
Timothy Fisher <timothyf@gmail.com>
2025-08-19 12:06:56 -0400

    Add top navigation bar with links

--- backend/apps/web/urls.py
+++ backend/apps/web/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('schedule/', views.schedule, name='schedule'),
+    path('standings/', views.standings, name='standings'),
+    path('players/', views.players, name='players'),
 ]
