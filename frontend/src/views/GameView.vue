6a563f974ddfa2d5561da30b0a45e4a420a9b467
Timothy Fisher <timothyf@gmail.com>
2025-08-20 18:14:19 -0400

    fixes

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -22,10 +22,31 @@ onMounted(async () => {
   if (resp.ok) {
     game.value = await resp.json();
   }
+
+  // fetch team names
+  const homeTeamId = game.value.team_home_id;
+  const awayTeamId = game.value.team_away_id;
+
+  // if (homeTeamId) {
+  //   const homeResp = await fetch(`/api/teams/${homeTeamId}/`);
+  //   if (homeResp.ok) {
+  //     game.value.homeTeamName = (await homeResp.json()).full_name;
+  //   }
+  // }
+
+  // if (awayTeamId) {
+  //   const awayResp = await fetch(`/api/teams${awayTeamId}/`);
+  //   if (awayResp.ok) {
+  //     game.value.awayTeamName = (await awayResp.json()).full_name;
+  //   }
+  // }
 });
 
-const homeTeam = computed(() => game.value?.teams?.home?.team?.name || '');
-const awayTeam = computed(() => game.value?.teams?.away?.team?.name || '');
+const homeTeam = computed(() => game.value.homeTeamName || '');
+const awayTeam = computed(() => game.value.awayTeamName || '');
+
+
+
 const homeScore = computed(() => game.value?.teams?.home?.score ?? '');
 const awayScore = computed(() => game.value?.teams?.away?.score ?? '');
 </script>
