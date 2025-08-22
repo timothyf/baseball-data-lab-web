d2bb4a65578bdc87b4d563b661c128394ff006bf
Timothy Fisher <timothyf@gmail.com>
2025-08-22 17:53:57 -0400

    updated home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <img src="/baseball-data-lab-logo.png" alt="Logo" />
+  <div class="home">
+    <img src="http://localhost:5173/logo.png" alt="Logo" />
   </div>
 </template>
+
 <script setup>
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  max-width: 800px;
+  margin: auto;
+}
+img {
+  max-width: 500px;
+}
+</style>
