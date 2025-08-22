e7c59ec8695a3f0e42fab3ca1dbd17b1c73701e0
Timothy Fisher <timothyf@gmail.com>
2025-08-22 16:46:13 -0400

    updated

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <h1>Home</h1>
-    <img :src="logo" alt="Logo" />
+    <img src="/baseball-data-lab-logo.png" alt="Logo" />
   </div>
 </template>
 <script setup>
-import logo from '/baseball-data-lab-logo.png'
 </script>
