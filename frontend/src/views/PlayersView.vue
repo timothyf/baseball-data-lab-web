c9ac11ce5963002006c57cdbeb00c2a3f7d8774a
Timothy Fisher <timothyf@gmail.com>
2025-08-23 15:12:57 -0400

    style: unify search page layout

--- frontend/src/views/PlayersView.vue
+++ frontend/src/views/PlayersView.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-    <h1>Players</h1>
-    <SearchAutocomplete
-      placeholder="Search for a player"
-      optionLabel="name_full"
-      searchEndpoint="/api/players/"
-      routeName="Player"
-    />
-  </div>
+  <section class="search-page players-view">
+    <div class="search-container">
+      <h1>Players</h1>
+      <SearchAutocomplete
+        placeholder="Search for a player"
+        optionLabel="name_full"
+        searchEndpoint="/api/players/"
+        routeName="Player"
+      />
+    </div>
+  </section>
 </template>
 
 <script setup>
 import SearchAutocomplete from '../components/SearchAutocomplete.vue';
 </script>
 
-<style scoped>
-</style>
 
