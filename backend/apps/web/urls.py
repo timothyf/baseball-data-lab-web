89d8f7dd1131bc27dc34e639e9130c62bfc43dbf
Timothy Fisher <timothyf@gmail.com>
2025-08-26 23:12:36 -0400

    feat: add developer template

--- backend/apps/web/urls.py
+++ backend/apps/web/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('team/<int:mlbam_team_id>/', views.team, name='team'),
     path('player/<int:player_id>/', views.player, name='player'),
     path('players/', views.players, name='players'),
+    path('developer/', views.developer, name='developer'),
     path('game/<int:game_pk>/', views.game, name='game'),
     path('new-game/<int:game_pk>/', views.new_game, name='new_game')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
