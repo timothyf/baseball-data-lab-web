67110495d558a0e5f121573c49acc3d257a28cb3
Timothy Fisher <timothyf@gmail.com>
2025-08-27 22:04:39 -0400

    Configure static file collection

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 pip install -r backend/requirements.txt \
  && cd frontend && npm ci && npm run build && cd .. \
- && python backend/manage.py collectstatic --noinput
+ && DJANGO_SETTINGS_MODULE=baseball_data_lab_web.settings.prod python backend/manage.py collectstatic --noinput
